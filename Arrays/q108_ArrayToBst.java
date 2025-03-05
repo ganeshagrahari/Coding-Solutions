2aac8287c758b368bc09b7c83c3eed182277060e
Ganesh Agrahari <142599651+ganeshagrahari@users.noreply.github.com>
2025-03-05 22:34:58 +0530

    question no. 108 array to bst...

--- Arrays/q108_ArrayToBst.java
+++ Arrays/q108_ArrayToBst.java
@@ -1,12 +1,12 @@
 
 
-public class TreeNode {
+public class q108_ArrayToBst {
     int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    q108_ArrayToBst left;
+    q108_ArrayToBst right;
+    q108_ArrayToBst() {}
+    q108_ArrayToBst(int val) { this.val = val; }
+    q108_ArrayToBst(int val, q108_ArrayToBst left, q108_ArrayToBst right) {
         this.val = val;
         this.left = left;
         this.right = right;
@@ -14,18 +14,18 @@ public class TreeNode {
 }
  
 class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
+    public q108_ArrayToBst sortedArrayToBST(int[] nums) {
         if(nums.length==0){
             return null;
         }
         return getbst(nums,0,nums.length-1);
     }
-    public TreeNode getbst(int nums [], int startIdx,int endIdx){
+    public q108_ArrayToBst getbst(int nums [], int startIdx,int endIdx){
         if(startIdx>endIdx){
             return null;
         }
         int midleIdx = (startIdx+endIdx)/2;
-        TreeNode bst = new TreeNode(nums[midleIdx]);
+        q108_ArrayToBst bst = new q108_ArrayToBst(nums[midleIdx]);
 
         bst.left =getbst(nums,startIdx,midleIdx-1 );
         bst.right=getbst(nums,midleIdx+1,endIdx);
