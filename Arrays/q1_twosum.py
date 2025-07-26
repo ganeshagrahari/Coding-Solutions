b972b7090166fe92b27f10aa9d234cc96ebe6c2b
ganeshagrahari <ganeshagrahari108@gmail.com>
2025-07-26 22:09:28 +0530

    Update two sum solution, add stack implementation and trees directory

--- Arrays/q1_twosum.py
+++ Arrays/q1_twosum.py
@@ -1,10 +1,18 @@
 class Solution:
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return None
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # return None
+        prevMap = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in prevMap:
+                return [prevMap[diff],i]
+            prevMap[num] = i
+        return        
+
 
 s1 = Solution()
 print(s1.twoSum([1,2,3,4,7],9))
